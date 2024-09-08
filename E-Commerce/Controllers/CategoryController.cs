d3f18aa413258bee0920b3781a29ded54f008155
Dev <iamdev1597@gmail.com>
2024-09-08 22:56:30 +0530

    Adding update and delete functionality

--- E-Commerce/Controllers/CategoryController.cs
+++ E-Commerce/Controllers/CategoryController.cs
@@ -39,5 +39,64 @@ namespace E_Commerce.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? idFromDb=_db.ProductCategory.FirstOrDefault(x=>x.id==id);
+            if (idFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(idFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and DisplayOrder can not be same!");
+            }
+            if (obj.Name != null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Invalid name!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.ProductCategory.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? idFromDb = _db.ProductCategory.FirstOrDefault(x => x.id == id);
+            if (idFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(idFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteId(int? id)
+        {
+           Category? deleteId= _db.ProductCategory.FirstOrDefault(x => x.id == id);
+            if(deleteId  == null)
+            {
+                return NotFound();
+            }
+            _db.ProductCategory.Remove(deleteId);
+            _db.SaveChanges();
+            return RedirectToAction("Index");            
+        }
     }
 }
